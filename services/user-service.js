790cc082c3d6f456947639a78d23b866f0a3e5e0
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2018-01-19 10:33:12 +0800

    Login function

---
+++ services/user-service.js
@@ -0,0 +1,27 @@
+const db = require('../knex-database-config');
+const client = require('../redis-database-config');
+const axios = require('axios');
+const UserModel = require('../models').users;
+const secret = require('../secret');
+const jwt = require('jwt-simple');
+
+const fbGraphAPI = 'https://graph.facebook.com/me?access_token=';
+
+module.exports = class UserService{
+    facebookLogin(token) {
+    return axios.get(`${fbGraphAPI}${token}`)
+        .then((data)=>{
+            console.log('data : ',data.data);
+            if(!data.data.error){
+                let userObj = new UserModel();
+                userObj.facebookId= data.data.id;
+                userObj.userName= data.data.name;
+
+                userObj.save().then((user) => {
+                    const output = jwt.encode(user, secret.jwtSecret);
+                    return output;
+                });
+            }
+        });
+    }
+}
